65cc116883aaa8eaf9ade577788e555540c84c2c
SergeiToptunov <toptunov.2005@mail.ru>
2023-10-25 20:01:27 +0300

    Решил 5 задач

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,4 @@
+// codewars link 1
 const evenOrOdd = (value) => {
     if (value % 2 ===0) {
         return "Even";
@@ -8,3 +9,28 @@ const evenOrOdd = (value) => {
 }
 console.log(evenOrOdd(1));
 console.log(evenOrOdd(2));
+
+// codewars link 3
+const getAge = (inputString) => {
+    return parseInt(inputString, 10);
+}
+const inputString = "1 год";
+const age = getAge(inputString);
+console.log(age);
+// codewars link 4
+const reverseList = (list) => {
+    return list.reverse();
+  }
+  const A = [1, 2, 3, 4];
+  const B = [9, 2, 0, 7];
+  
+  const reversedA = reverseList(A);
+  const reversedB = reverseList(B);
+
+  console.log(reversedA);
+  console.log(reversedB);
+  // codewars link 5
+  const twiceAsOld = (dadYearsOld, sonYearsOld) => {
+    return Math.abs(dadYearsOld - 2 * sonYearsOld);
+  }
+  console.log(twiceAsOld(30, 5));
