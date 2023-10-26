8278300bebc4961d15a7db36887c1f172d0c9c95
SergeiToptunov <toptunov.2005@mail.ru>
2023-10-26 15:37:26 +0300

    +plug

--- src/functions.js
+++ src/functions.js
@@ -1,36 +1,30 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line linebreak-style
+/* eslint-disable no-console */
+/* eslint-disable linebreak-style */
 // codewars link 1
 const evenOrOdd = (value) => {
-    if (value % 2 ===0) {
-        return "Even";
-    } else {
-        return "Odd";
-    }
-
-}
+  if (value % 2 === 0) {
+    return 'Even';
+  }
+  return 'Odd';
+};
 console.log(evenOrOdd(1));
 console.log(evenOrOdd(2));
 
 // codewars link 3
-const getAge = (inputString) => {
-    return parseInt(inputString, 10);
-}
-const inputString = "1 год";
+const getAge = (inputString) => parseInt(inputString, 10);
+const inputString = '1 год';
 const age = getAge(inputString);
 console.log(age);
 // codewars link 4
-const reverseList = (list) => {
-    return list.reverse();
-  }
-  const A = [1, 2, 3, 4];
-  const B = [9, 2, 0, 7];
-  
-  const reversedA = reverseList(A);
-  const reversedB = reverseList(B);
-
-  console.log(reversedA);
-  console.log(reversedB);
-  // codewars link 5
-  const twiceAsOld = (dadYearsOld, sonYearsOld) => {
-    return Math.abs(dadYearsOld - 2 * sonYearsOld);
-  }
-  console.log(twiceAsOld(30, 5));
+const reverseList = (list) => list.reverse();
+const A = [1, 2, 3, 4];
+const B = [9, 2, 0, 7];
+const reversedA = reverseList(A);
+const reversedB = reverseList(B);
+console.log(reversedA);
+console.log(reversedB);
+// codewars link 5
+const twiceAsOld = (dadYearsOld, sonYearsOld) => Math.abs(dadYearsOld - 2 * sonYearsOld);
+console.log(twiceAsOld(30, 5));
